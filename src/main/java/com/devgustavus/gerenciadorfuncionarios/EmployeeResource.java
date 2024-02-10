f161ab0b45a9ccb9f8d84cb4bf628eec50c8faa6
DevGustavus <gustavusinvictus@gmail.com>
2024-02-09 21:42:45 -0300

    atualizando projeto

--- src/main/java/com/devgustavus/gerenciadorfuncionarios/EmployeeResource.java
+++ src/main/java/com/devgustavus/gerenciadorfuncionarios/EmployeeResource.java
@@ -1,8 +1,19 @@
 package com.devgustavus.gerenciadorfuncionarios;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devgustavus.gerenciadorfuncionarios.Model.Employee;
 import com.devgustavus.gerenciadorfuncionarios.Service.EmployeeService;
 
 @RestController
@@ -14,5 +25,33 @@ public class EmployeeResource {
 		this.employeeService = employeeService;
 	}
 	
+	@GetMapping("/all")
+	public ResponseEntity<List<Employee>> getAllEmployees () {
+		List<Employee> employees = employeeService.findAllEmployees();
+		return new ResponseEntity<>(employees, HttpStatus.OK);
+	}
 	
+	@GetMapping("/find/{id}")
+	public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id) {
+		Employee employee = employeeService.findEmployeeById(id);
+		return new ResponseEntity<>(employee, HttpStatus.OK);
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Employee> addEployee(@RequestBody Employee employee) {
+		Employee newEmployee = employeeService.addEmployee(employee);
+		return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Employee> updateEployee(@RequestBody Employee employee) {
+		Employee updateEmployee = employeeService.updateEmployee(employee);
+		return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> deleteEployee(@PathVariable("id") Long id) {
+		employeeService.deleteEmployee(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 }
--- src/main/java/com/devgustavus/gerenciadorfuncionarios/GerenciadorFuncionariosApplication.java
+++ src/main/java/com/devgustavus/gerenciadorfuncionarios/GerenciadorFuncionariosApplication.java
@@ -2,6 +2,9 @@ package com.devgustavus.gerenciadorfuncionarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GerenciadorFuncionariosApplication {
@@ -9,5 +12,16 @@ public class GerenciadorFuncionariosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorFuncionariosApplication.class, args);
 	}
-
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
